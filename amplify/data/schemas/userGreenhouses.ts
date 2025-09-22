a31059412eb66de3b5ae1bde15ca45a58b549c2b
swgrouop <akai@ratocsystems.com>
2025-09-22 11:43:53 +0900

    ・userGreenhouses.tsの修正

--- amplify/data/schemas/userGreenhouses.ts
+++ amplify/data/schemas/userGreenhouses.ts
@@ -1,14 +1,19 @@
-import { a } from '@aws-amplify/backend';
-export const userGreenhousesModel = a
-    .model({
-        userId: a.string().required(),
-        greenhouseId: a.string().required(),
-        greenhouseName: a.string(),
-        createdAt: a.datetime(),
-        // センサーIDのリスト
-        soilSensors: a.listOf(a.string()),
-        co2Sensors: a.listOf(a.string()),
-        solarSensors: a.listOf(a.string()),
-    })
-    .identifier(['userId', 'greenhouseId'])
-    .authorization((allow) => [allow.publicApiKey(), allow.authenticated()]);
+import { defineData } from '@aws-amplify/backend';
+
+export const data = defineData({
+    UserGreenhouses: (model) =>
+        model
+            .identifier(['userId', 'greenhouseId'])
+            .fields({
+                userId: model.string().required(),
+                greenhouseId: model.string().required(),
+                greenhouseName: model.string(),
+                createdAt: model.datetime(),
+
+                // 正しい配列型の定義
+                soilSensors: model.listOf(model.string()),
+                co2Sensors: model.listOf(model.string()),
+                solarSensors: model.listOf(model.string()),
+            })
+            .authorization((allow) => [allow.authenticated()]),
+});
